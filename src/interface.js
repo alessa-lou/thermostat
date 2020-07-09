ee6bc6d447bb6a04cf578ea1d7926ebd1f9fd1ba
Miri Michaelson <miriammichaelson@gmail.com>
2020-07-09 17:26:21 +0100

    added jquery functionality

--- src/interface.js
+++ src/interface.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('.temperature').text(thermostat.temperature);
-})
+  $('#temperature').text(thermostat.temperature);
 
-console.log("right before click function")
-
-console.log($("[name='up']"))
-
-// $("[name='up']").on('click'(function() {
-  $('.up').click(function() {
+  $('#up').click(function() {
+  var thermostat = new Thermostat();
   console.log("click worked good job")
   thermostat.up();
-  $('.temperature').text(thermostat.temperature);
+  $('#temperature').text(thermostat.temperature);
+});
+
+$('#down').click(function() { // this is an alternate version of .on('click'), with a sprinkle of jQuery syntactic sugar
+  var thermostat = new Thermostat();
+  thermostat.down();
+  $('#temperature').text(thermostat.temperature);
+})
+
 })
