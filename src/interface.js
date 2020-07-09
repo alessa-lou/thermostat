8ac420bb98e00dc4ad9c37b5eee1b10da1b3db8e
Miri Michaelson <miriammichaelson@gmail.com>
2020-07-09 16:55:29 +0100

    asking 4 help

--- src/interface.js
+++ src/interface.js
@@ -2,3 +2,14 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   $('.temperature').text(thermostat.temperature);
 })
+
+console.log("right before click function")
+
+console.log($("[name='up']"))
+
+// $("[name='up']").on('click'(function() {
+  $('.up').click(function() {
+  console.log("click worked good job")
+  thermostat.up();
+  $('.temperature').text(thermostat.temperature);
+})
